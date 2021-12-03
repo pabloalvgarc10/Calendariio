5867cef86fe2ef543eabb4024b7b2b98411f27c2
Pablo <pabloalvgar10@gmail.com>
2021-12-03 13:52:48 +0100

    02

---
+++ CalendarioBasico.java
@@ -0,0 +1,13 @@
+
+public class CalendarioBasico{
+   private int dia;
+   private int mes;
+   private int año;
+
+
+    public CalendarioBasico(){
+        dia = 1;
+        mes = 1;
+        año = 1;
+    }
+}
